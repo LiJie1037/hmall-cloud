a0e55310ac30e54c69e08d265ea618ae4d905395
LiJie1037 <2363410296@qq.com>
2024-06-24 10:01:19 +0800

    第一次提交,使用openFeign

---
+++ item-service/src/main/java/com/hmall/item/ItemApplication.java
@@ -0,0 +1,18 @@
+package com.hmall.item;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version 1.0
+ * @Author Lee
+ * @Date 2024/6/23 16:57
+ */
+@MapperScan("com.hmall.item.mapper")
+@SpringBootApplication
+public class ItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItemApplication.class, args);
+    }
+}
